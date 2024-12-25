eea187b03180ea6d756a9ea95f556ff5c6e7fe19
Akame style <eiljo@mail.ru>
2024-12-25 11:18:09 +0300

    123

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+import React, { useState, useEffect, use } from 'react';
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function App() {
+  const[offset, setOffset] = useState(0);
+  const[limit, setLimit] = useState(4);
+  const[data, setData] = useState([]);
+  const[isLoading, setIsLoading] = useState(false);
+  
+function CreateCard(title)
+{
+  return(
+    <Card style={{ width: '15rem' }}>
+      <Card.Body>
+        <Card.Title>{title.title.name}</Card.Title>
+        <Card.Link href={title.title.url}>Link</Card.Link>
+      </Card.Body>
+    </Card>
+    )
+}
+
+    const check = async() =>
+  {
+    try
+      {
+        let res = await fetch(`https://pokeapi.co/api/v2/pokemon/?name=&limit=${limit}&offset=${offset}`).then((res) => res.json());
+        setData(res.results);
+      }
+    catch(error)
+      {
+        console.error('Error fetch API data');
+      }
+    finally
+      {
+        setIsLoading(true);
+      }
+  }
+
+  if(!data)
+    {
+      setIsLoading(true)
+    }  
+  if(!isLoading)
+    {
+      check();
+    }
+  console.log(data)
+
+  const incOffset = () =>
+    {
+      setOffset(offset+5);
+      check();
+    }
+
+
+  const decOffset = () =>
+    {
+      if(offset === 0)
+      {
+        return
+      }
+      setOffset(offset-5);
+      check();
+
+    }
+  
+  console.log(offset)
+  return (
+    <div>
+      <Container>
+        <Row xl = {4} md sm = {5} xs = {1}>
+            {
+              data.map((item, index) =>
+                (
+                  <Col key={index}>
+                    <CreateCard title={item}/>
+                  </Col>
+                )
+              )
+            }
+        </Row>
+      
+      </Container>
+      <button id='back' onClick={decOffset}>Previous</button>
+      <button id='next' onClick={incOffset}>Next</button>
+      <div id='pagin'>Page:{offset / 5}</div>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/cards.js
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card';
+
+function CreateCard()
+    {
+      return(
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Link href="#">Card Link</Card.Link>
+          </Card.Body>
+        </Card>
+        )
+    }
+
+function RenderCards(jsonData)
+{
+    return(
+    <div id='cards'>
+    <CreateCard/>
+    <CreateCard/>
+    <CreateCard/>
+    <CreateCard/>
+    <CreateCard/>
+    </div>
+    )
+}
+export default RenderCards;
